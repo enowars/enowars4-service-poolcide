9b2e1947e12bdb344351a76f5ec4235ac6841595
M:Munier <marc@geomaps.de>
2020-06-28 01:16:19 +0200

    fix su

--- service/start.sh
+++ service/start.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 mkdir -p /data/cookies /data/priority_towels /data/towels /data/users;
-chown -R poolcide /data;
-su poolcide;
-python3 poolcgid.py;
+chown -R poolcide:poolcide /data;
+su poolcide:poolcide -c "python3 poolcgid.py";
