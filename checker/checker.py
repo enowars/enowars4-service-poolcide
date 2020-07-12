b1fb0836d7faee4a83e9b7f6e629ef8077071c67
Dominik Maier <domenukk@gmail.com>
2020-07-12 10:50:02 +0200

    fixed checker

--- checker/checker.py
+++ checker/checker.py
@@ -17,20 +17,34 @@ AGE_SECRET_KEY = (
 AGE_KEYFILE = "./age_key"
 
 with open("users.txt") as f:
-    # users should not include '=', or '&' chars
+    # users must not include '=', or '&' chars
     users = [x.strip() for x in f.readlines()]
 
+assert(len(users))
 
+
+# noinspection PyDefaultArgument
 def build_http(
     route: str = "/cgi-bin/poolcide",
     method: str = "GET",
+    params: Dict[str, str] = {},
     query_params: Dict[str, str] = {},
     body_params: Dict[str, str] = {},
     cookies: Dict[str, str] = {},
 ) -> str:
     """
+    anything passed in params parameter goes either to query or body,
     Builds a http request, with shuffled get, body, and cookie, positions.
     """
+    query_params = query_params.copy()
+    body_params = body_params.copy()
+
+    for key, val in params.items():
+        if secrets.randbelow(2) == 0:
+            query_params[key] = val
+        else:
+            body_params[key] = val
+
     shuffled_gets = list(query_params.keys())
     random.shuffle(shuffled_gets)
     shuffled_body = list(body_params.keys())
@@ -50,8 +64,9 @@ def build_http(
             # divide with \n
             body = f"{body}\n{body_key}={body_val}"
             
-    # Make sure to always end on \n - else the connection may block :(
-    body += "\n"  
+    # Make sure to always end params on \n - else the connection may block (params are read line by line)
+    if body != "":
+        body += "\n"
 
     shuffled_cookies = list(cookies.keys())
     if len(shuffled_cookies):
@@ -93,8 +108,11 @@ class PoolcideChecker(BaseChecker):
         return cookie
 
     def user_request(
-        self, route: str, cookie, csrf: str, username: str, password: str
-    ) -> str:
+        self, route: str, cookie: str, csrf: str, username: str, password: str
+    ) -> Tuple[str, str, str]:
+        """
+        return: Tuple(response, cookie, csrf)
+        """
         self.info(f"Executing {route} as user {username} with password {password}")
         with self.connect() as t:
             http = build_http(
@@ -105,33 +123,46 @@ class PoolcideChecker(BaseChecker):
             )
             self.debug(f"Sending request: {http}")
             t.write(http)
-            resp = t.read_all()
+            resp = ensure_unicode(t.read_all())
             self.debug(f"Got response: {resp}")
             new_cookie = self.parse_cookie(resp)
 
             assert_equals(cookie, new_cookie)
 
-            if not b"success" in resp:
+            if "success" not in resp:
                 self.error(f"No success response, instead got {resp}")
                 raise BrokenServiceException("Login failed")
             self.info(f"Got cookie {cookie}")
-            return cookie
+            return resp, cookie, csrf
 
-    def login(self, cookie: str, csrf: str, username: str, password: str) -> str:
+    def login(self, cookie: str, csrf: str, username: str, password: str) -> Tuple[str, str, str]:
         return self.user_request("login", cookie, csrf, username, password)
 
-    def register(self, cookie: str, csrf: str, username: str, password: str) -> str:
+    def register(self, cookie: str, csrf: str, username: str, password: str) -> Tuple[str, str, str]:
         return self.user_request("register", cookie, csrf, username, password)
 
     def reserve_as_admin(self, cookie: str) -> None:
-        # TODO
         color_string = urllib.parse.quote(self.flag)
         with self.connect() as t:
-            t.write(
-                f"POST /cgi-bin/poolcide?route=reserve HTTP/1.0\r\n"
-                f"Cookie: poolcode={cookie}\r\n\r\n"
-                f"color={color_string}\n"
+            http = build_http(
+                method="GET",
+                query_params={"route": "dispense"},
+                cookies={"poolcode": cookie}
             )
+            self.debug(f"request for dispense: {http}")
+            t.write(http)
+            resp = ensure_unicode(t.read_all())
+            self.debug(f"response for dispense was {resp}")
+
+        try:
+            csrf = resp.split('name="csrf" value="')[1].split('"')[0]
+        except Exception as ex:
+            self.warning(f"Could not read csrf token: {ex}")
+            raise BrokenServiceException("No csrf token found in route=dispense")
+
+        with self.connect() as t:
+            http = build_http(method="POST", query_params={"route": "reserve"}, params={"color": color_string, "csrf": csrf}, cookies={"poolcode": cookie})
+            t.write(http)
             stuff = t.read_until("<code>")
             # TODO expect more stuff
             if b"admin" not in stuff:
@@ -223,7 +254,8 @@ class PoolcideChecker(BaseChecker):
             raise BrokenServiceException("Cookie could not be found!")
 
         self.info("trying to log in")
-        cookie = self.register(cookie, csrf, user, password)
+        resp, cookie, csrf = self.register(cookie, csrf, user, password)
+        self.info(f"Logged in as {user}")
 
         self.reserve_as_admin(cookie)
 
@@ -241,16 +273,18 @@ class PoolcideChecker(BaseChecker):
         req = build_http(query_params={"route": "index"})
         with self.connect() as sock:
             sock.write(req)
-            indexresp = sock.read_all()
+            indexresp = ensure_unicode(sock.read_all())
         # find <input type="hidden" id="csrf" name="csrf" value="7XT3cepo" />
         try:
-            csrf = indexresp.text.split('name="csrf" value="')[1].split('" />')[0]
+            csrf = indexresp.split('name="csrf" value="')[1].split('" />')[0]
             self.info(f"Got csrf token {csrf}")
         except Exception as ex:
             self.warning("Could not find csrf token", exc_info=ex)
             raise BrokenServiceException("csrf token could not be found!")
 
-        cookie = self.login(csrf, user, password)
+        cookie = self.parse_cookie(indexresp)
+
+        resp, cookie, csrf = self.login(cookie, csrf, user, password)
         resp = self.get_towel(cookie, towel_token)
         try:
             escaped_flag = resp.split('<code id="color">')[1].split("</code>")[0]
